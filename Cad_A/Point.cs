ff4a2efe91dbf8a8897d54837207ca5d670b1768
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-04-24 15:32:29 +0200

    Add project files.

---
+++ Cad_A/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace Cad_A
+{
+  public class Point
+  {
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Point(double x, double y)
+    {
+      X = x;
+      Y = y;
+    }
+
+    public void AddToCanvas(Canvas canvas)
+    {
+      Ellipse ellipse = new Ellipse();
+      ellipse.Fill = Brushes.Black;
+      ellipse.Width = 8;
+      ellipse.Height = 8;
+      Canvas.SetLeft(ellipse, X - 4);
+      Canvas.SetTop(ellipse, Y - 4);
+      canvas.Children.Add(ellipse);
+    }
+  }
+}
